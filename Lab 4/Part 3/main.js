438ed64a8bf51786c75fa9560e38a6d18f0da5b3
Immortal0787 <101957715+Immortal0787@users.noreply.github.com>
2024-04-05 17:34:10 -0400

    Gallery of Photots
    The code creates a gallery of photos with the featured photo, a button for the darkening of the featured photo, and a placeholder to show all the thumbnail photos of the gallery. If you want, you can freely redesign and add more photos to make it a gallery of your own.

--- Lab 4/Part 3/main.js
+++ Lab 4/Part 3/main.js
@@ -1,19 +1,93 @@
-// setup canvas
-
+// Setting up the canvas
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+// Function to generate a random number
+function getRandomNumber(min, max) {
+  const number = Math.floor(Math.random() * (max - min + 1)) + min;
+  return number;
 }
 
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+// Function to generate a random color
+function getRandomRGB() {
+  return `rgb(${getRandomNumber(0, 255)},${getRandomNumber(0, 255)},${getRandomNumber(0, 255)})`;
 }
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  detectCollision() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = getRandomRGB();
+        }
+      }
+    }
+  }
+}
+
+const balls = [];
+
+while (balls.length < 10) {
+  const size = getRandomNumber(10, 20);
+  const ball = new Ball(
+    getRandomNumber(0 + size, width - size),
+    getRandomNumber(0 + size, height - size),
+    getRandomNumber(-7, 7),
+    getRandomNumber(-7, 7),
+    getRandomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+function animationLoop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+  
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.detectCollision();
+  }
+  
+  requestAnimationFrame(animationLoop);
+}
+
+animationLoop();

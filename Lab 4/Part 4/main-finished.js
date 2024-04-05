72c2e63d524ffbb9ad5c3389b17572909698bd3f
Immortal0787 <101957715+Immortal0787@users.noreply.github.com>
2024-04-05 18:05:23 -0400

    Boundaries
    modifies the position of an object (this.x and this.y) based on its velocity (this.velX and this.velY), and ensures that it bounces off the canvas edges when reaching the boundaries defined by width and height

--- Lab 4/Part 4/main-finished.js
+++ Lab 4/Part 4/main-finished.js
@@ -21,13 +21,14 @@ function randomRGB() {
 }
 
 class Ball {
-  constructor(x, y, velX, velY, color, size) {
+  constructor(x, y, velX, velY, color, size, exist) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
     this.color = color;
     this.size = size;
+    this.exist = exist;
   }
 
   draw() {
@@ -36,72 +37,14 @@ class Ball {
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
-    }
-
-    if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
-    }
-
-    if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
-    }
-
-    if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
-    }
-
     this.x += this.velX;
     this.y += this.velY;
-  }
 
-  collisionDetect() {
-    for (const ball of balls) {
-      if (!(this === ball)) {
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (distance < this.size + ball.size) {
-          ball.color = this.color = randomRGB();
-        }
-      }
+    if (this.size + this.x >= width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+    if (this.size + this.y >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
     }
   }
-}
-
-const balls = [];
-
-while (balls.length < 25) {
-  const size = random(10, 20);
-  const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    randomRGB(),
-    size
-  );
-
-  balls.push(ball);
-}
-
-function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
-  ctx.fillRect(0, 0, width, height);
-
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-
-  requestAnimationFrame(loop);
-}
-
-loop();
